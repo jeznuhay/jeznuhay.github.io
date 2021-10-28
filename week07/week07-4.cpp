6cbbcafb08419d742889e2ba8245ef95fd23231e
Jean Hu <91250372+jeznuhay@users.noreply.github.com>
2021-10-28 17:40:28 +0800

    Add files via upload

---
+++ week07/week07-4.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    for(i=0; i<=3; i++ )
+    {
+    printf("課本的寫法喔\n");
+    }
+
+    for(int i=0; i<3; i++)
+    {
+    printf("小葉老師的寫法喔\n");
+    }
+
+
+
+
+}
---
+++ week07/week07-5.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=0; i<3; i++)
+    {
+    printf("電腦迴圈最好的寫法:%d\n", i);
+    }
+
+    for(int i=1; i<=3; i++)
+    {
+    printf("人類迴圈最好的寫法:%d\n", i);
+    }
+
+    for(int i=0; i<=3; i++)
+    {
+    printf("不好的寫法:%d\n", i);
+    }
+
+
+
+
+
+
+
+}
