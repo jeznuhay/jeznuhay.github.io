07a18614eebd7c9bfbe44998a6ef6457da7b9afe
Jean Hu <91250372+jeznuhay@users.noreply.github.com>
2021-11-04 17:35:34 +0800

    Add files via upload

---
+++ week08/week08-5.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    print("1: *\n");
+    print("2: **\n");
+    print("3: ***\n");
+    print("4: ****\n");
+
+    int n=4;
+    for( int i=1; i<=4; i++)
+        printf("%d: \n", i);
+
+
+
+
+
+
+}
