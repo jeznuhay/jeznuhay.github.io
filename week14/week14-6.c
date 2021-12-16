882f003d20d354645cce18f801de58f573704b55
Jean Hu <91250372+jeznuhay@users.noreply.github.com>
2021-12-16 17:37:20 +0800

    Add files via upload

---
+++ week14/week14-6.c
@@ -0,0 +1,17 @@
+	int n;
+	scanf("%d", &n);
+
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			scanf("%d", &a[i][j] );
+		}
+	}
+
+	printf("\n");
+	for(int j=0; j<n; j++){
+		for(int i=0; i<n; i++){
+			printf("%3d", a[i][j]);
+		}
+		printf("\n");
+	}
+}
